10d4c3ebd701f74944b9ef13c033d6ef327c9f9a
Zain Sarfraz <zainsarfraz82@gmail.com>
2024-03-06 21:52:12 +0500

    Class-Test

---
+++ Class_Task_Assignment/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Give Array
+int[] arr = { 1, 3, 42, 20, 97, 65, 88, 43, 64, 20, 12, 48, 24, 38 };
+
+// Find the sum of all the integers in the array
+int sum = arr.Sum();
+Console.WriteLine($"Sum of all integers: {sum}");
+
+// Find the maximum and minimum values in the array
+int max = arr.Max();
+int min = arr.Min();
+Console.WriteLine($"\nMaximum value: {max}");
+Console.WriteLine($"\nMinimum value: {min}");
+
+// Count the number of even and odd integers in the array
+int evenCount = arr.Count(num => num % 2 == 0);
+int oddCount = arr.Count(num => num % 2 != 0);
+Console.WriteLine($"\nNumber of even integers: {evenCount}");
+Console.WriteLine($"\nNumber of odd integers: {oddCount}");
+
+// Calculate the average of all the integers in the array
+double average = arr.Average();
+Console.WriteLine($"\nAverage of all integers: {average}");
+
+// Check if a specific integer is present in the array
+int specificNumber = 20;
+bool isPresent = arr.Contains(specificNumber);
+Console.WriteLine($"\n{specificNumber} is present in the array: {isPresent}\n");
+
+
+// Create a two-dimensional array
+int[,] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+int rows = matrix.GetLength(0);
+int cols = matrix.GetLength(1);
+
+int sumdarr = 0;
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        //sum of all elements in a two-dimensional array (matrix).
+        Console.WriteLine($"sum of all elements in a two-dimensional array (matrix): {sumdarr += matrix[i, j]}");
+    }
+}
---
+++ Function/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Length and width of the rectangle
+            double length = 20;
+            double width = 12;
+
+            // Formula for calculating the area of a rectangle
+            double area = length * width;
+
+            // Display the result
+            Console.WriteLine($"Area of the rectangle: {area} square centimeters");
+        }
+    }
+}
